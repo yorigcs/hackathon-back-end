446049f39121e0534b25ad15ee83878ee08a450a
Yorí <yorigcsdev@gmail.com>
2022-07-15 04:47:07 -0300

    Feature: add signUpController

---
+++ src/controllers/publicControllers/signUpController.js
@@ -0,0 +1,14 @@
+import mongoDB from "../../database/mongoDB.js";
+
+const signUpController = async (req, res) => {
+    const { signUpData } = res.locals;
+    try {
+        await mongoDB.collection("users").insertOne(signUpData);
+        res.sendStatus(201);
+    } catch (err) {
+        console.error(err);
+        return res.status(500).send("Houve um erro ao criar o usuário!")
+    }
+};
+
+export default signUpController;
